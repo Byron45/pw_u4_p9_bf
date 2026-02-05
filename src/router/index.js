6a164f6410522f7750e8e15f9b9909086b453900
Byron45 <brfloresa@uce.edu.ec>
2026-02-05 18:44:58 -0500

    Taller 36

--- src/router/index.js
+++ src/router/index.js
@@ -11,48 +11,94 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/listar',
     name: 'ListarTodos',
-    component: ListarTodosView
+    component: ListarTodosView,
+    meta: {
+      requiereAutorizacion: false,
+      esPublica: true
+    }
   },
   {
     path: '/listarporid',
     name: 'ListarPorId',
-    component: ListarPorIdView
+    component: ListarPorIdView,
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/guardar',
     name: 'Guardar',
-    component: GuardarView
+    component: GuardarView,
+    meta: {
+      requiereAutorizacion: false,
+      esPublica: true
+    }
   },
   {
     path: '/actualizar',
     name: 'Actualizar',
-    component: ActualizarView
+    component: ActualizarView,
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/actualizarparcial',
     name: 'ActualizarParcial',
-    component: ActualizarParcialView
+    component: ActualizarParcialView,
+    meta: {
+      requiereAutorizacion: false,
+      esPublica: true
+    }
   },
   {
     path: '/borrar',
     name: 'Borrar',
-    component: BorrarView
+    component: BorrarView,
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+/*Configuracion del Guardian*/
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiereAutorizacion) {
+    /*Le envio a una pagina de Login*/
+    console.log('Redirigiendo a Login...');
+
+  } else {
+    /*Le dejo pasar sin validaciones*/
+    console.log('Pase Libre');
+    next();
+  }
+
 })
 
 export default router
